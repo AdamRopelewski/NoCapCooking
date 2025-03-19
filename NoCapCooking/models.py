d489ecebd5213f395c3cded8f3f4183f276444d3
Tomasz Niewiadomski <149410768+tomiNiewiadomski@users.noreply.github.com>
2025-03-18 17:47:38 -0700

    Update models.py

--- NoCapCooking/models.py
+++ NoCapCooking/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
 
-class Tag(models.Model):
+""" class Tag(models.Model):
+    name = models.CharField(max_length=50) """
+
+class Photo(models.Model):
+    URL = models.CharField(max_length=50)
+
+class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
+class Cuisine(models.Model):
+    name = models.CharField(max_length=50)
+
+class Diet(models.Model):
+    name = models.CharField(max_length=50)
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
-    tags = models.ManyToManyField(Tag)
+    ingredients = models.ManyToManyField(Ingredient)
     instructions = models.TextField()
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE) 
+    diets = models.ManyToManyField(Diet)
+    photos = models.OneToOneField(Photo)    
