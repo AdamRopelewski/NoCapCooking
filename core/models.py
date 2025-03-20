71c383064b22a162d128e50ce5ed5210593cc3f4
Dawid Maliszewski <dawid@thedavesky.com>
2025-03-20 09:29:01 +0000

    Enhance models and migrations and add Pillow dependency for image handling

--- core/models.py
+++ core/models.py
@@ -1,25 +1,49 @@
 from django.db import models
 
 
-""" class Tag(models.Model):
-    name = models.CharField(max_length=50) """
-
-class Photo(models.Model):
-    URL = models.CharField(max_length=50)
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=50)
-
 class Cuisine(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(
+        max_length=50, help_text="Name of the cuisine (e.g., Italian)"
+    )
+
 
 class Diet(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(
+        max_length=50, help_text="Dietary category (e.g., Omnivore, Carnivore)"
+    )
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=50, help_text="Name of the ingredient (e.g., Spaghetti)"
+    )
+
+
+class Photo(models.Model):
+    image = models.ImageField(
+        upload_to="recipe_photos/", help_text="Photo representing the recipe"
+    )
+
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=50)
-    ingredients = models.ManyToManyField(Ingredient)
-    instructions = models.TextField()
-    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE) 
-    diets = models.ManyToManyField(Diet)
-    photos = models.OneToOneField(Photo, on_delete=models.CASCADE)
+    name = models.CharField(
+        max_length=100,
+        help_text="Name of the recipe (e.g., Spaghetti Carbonara)",
+    )
+    cuisine = models.ForeignKey(
+        Cuisine,
+        on_delete=models.CASCADE,
+        help_text="Cuisine associated with the recipe",
+    )
+    diet = models.ManyToManyField(
+        Diet, help_text="Dietary categories that apply to the recipe"
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient, help_text="Ingredients used in the recipe"
+    )
+    recipe = models.TextField(
+        help_text="Step-by-step instructions for preparing the recipe"
+    )
+    photo = models.OneToOneField(
+        Photo, on_delete=models.CASCADE, help_text="Photo for the recipe"
+    )
