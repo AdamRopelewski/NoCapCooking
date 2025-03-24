f74e1cc368aaea3d9c72168aac2297b4fa630ce6
Adam Ropelewski <adam.ropelewski.kontakt@gmail.com>
2025-03-24 17:14:50 +0000

    WIP: Refactor filter_recipes function to improve query handling for positive and negative filtering

--- core/views.py
+++ core/views.py
@@ -54,75 +54,6 @@ def get_recipes(request):
         amount = DEFAULT_AMOUNT
     return JsonResponse(recipes[:amount], safe=False)
 
-
-
-
-# def filter_recipes(request):
-#     all_filter_params = [
-#     'cuisine', 'diet', 'ingredient',
-#     'exclude_cuisine', 'exclude_diet', 'exclude_ingredient'
-#     ]
-    
-#     # Sprawdź czy są jakiekolwiek parametry filtrowania
-#     if not any(param in request.GET for param in all_filter_params):
-#         example_url = (
-#             "/recipes/filter/?cuisine=italian&diet=vegetarian&ingredient=tomato"
-#             "&exclude_ingredient=onion"
-#         )
-#         return JsonResponse({
-#             "message": "No filters provided. Here's an example query:",
-#             "example_query": example_url,
-#             "available_filters": {
-#                 "inclusive": ["cuisine", "diet", "ingredient"],
-#                 "exclusive": ["exclude_cuisine", "exclude_diet", "exclude_ingredient"]
-#             }
-#         }, status=400)
-    
-#     recipes = Recipe.objects.all()
-
-#     # Filtrowanie pozytywne
-
-#     cuisines = request.GET.getlist('cuisine')
-#     if cuisines:
-#         recipes = recipes.filter(cuisine__name__in=cuisines)
-
-#     diets = request.GET.getlist('diet')
-#     if diets:
-#         recipes = recipes.filter(diet__name__in=diets)
-
-#     ingredients = request.GET.getlist('ingredient')
-#     if ingredients:
-#         recipes = recipes.filter(ingredients__name__in=ingredients)
-
-
-
-
-#     # Filtrowanie negatywne - wykluczanie przepisów, które mają podane tagi.
-    
-#     exclude_cuisines = request.GET.getlist('exclude_cuisine')
-#     if exclude_cuisines:
-#         recipes = recipes.exclude(cuisine__name__in=exclude_cuisines)
-
-#     exclude_diets = request.GET.getlist('exclude_diet')
-#     if exclude_diets:
-#         recipes = recipes.exclude(diet__name__in=exclude_diets)
-
-#     exclude_ingredients = request.GET.getlist('exclude_ingredient')
-#     if exclude_ingredients:
-#         recipes = recipes.exclude(ingredients__name__in=exclude_ingredients)
-
-
-
-
-#     recipes = recipes.distinct()
-
-#     serialized_recipes = []
-#     for recipe in recipes:
-#         serialized_recipes.append(recipe_serializer(recipe))
-
-#     return JsonResponse(serialized_recipes, safe=False)
-
-
 def filter_recipes(request):
     # Lista dostępnych parametrów filtrowania
     filter_params = [
@@ -148,20 +79,20 @@ def filter_recipes(request):
     # Budujemy obiekt Q do filtrowania zapytań
     query = Q()
 
-    # Filtrowanie pozytywne
+    # Filtrowanie pozytywne (wymagaj wszystkich podanych wartości)
     cuisines = request.GET.getlist('cuisine')
-    if cuisines:
-        query &= Q(cuisine__name__in=cuisines)
+    for cuisine in cuisines:
+        query &= Q(cuisine__name=cuisine)
 
     diets = request.GET.getlist('diet')
-    if diets:
-        query &= Q(diet__name__in=diets)
+    for diet in diets:
+        query &= Q(diet__name=diet)
 
     ingredients = request.GET.getlist('ingredient')
-    if ingredients:
-        query &= Q(ingredients__name__in=ingredients)
+    for ingredient in ingredients:
+        query &= Q(ingredients__name=ingredient)
     
-    # Filtrowanie negatywne (wykluczające)
+    # Filtrowanie negatywne (wyklucz jakąkolwiek podaną wartość)
     exclude_cuisines = request.GET.getlist('exclude_cuisine')
     if exclude_cuisines:
         query &= ~Q(cuisine__name__in=exclude_cuisines)
