d84ae7874f9377edf66d278fbc3a0c3353de7cd1
Adam Ropelewski <adam.ropelewski.kontakt@gmail.com>
2025-03-24 13:27:16 +0000

    Add filter_recipes endpoint to recipe URL routing

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,13 +1,16 @@
-# myapp/urls.py
 from django.urls import path
-from core.views import (current_datetime, get_cuisines, 
-                        get_ingredients, get_diets, get_recipes)
-# app_name = 'api'  # This defines the app's namespace
+from core.views import (
+    get_cuisines, 
+    get_ingredients, 
+    get_diets, 
+    get_recipes,
+    filter_recipes,  
+)
 
 urlpatterns = [
-    path("time/", current_datetime, name="current_datetime"),
     path("cuisines/", get_cuisines, name="get_cuisines"),
     path("diets/", get_diets, name="get_diets"),
     path("ingedients/", get_ingredients, name="get_ingedients"),
     path("recipes/", get_recipes, name="get_recipes"),
+    path("recipes/filter/", filter_recipes, name="filter_recipes"),  
 ]
